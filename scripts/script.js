fe94d49e7ff6ce30470b649c7467c4c9ced4f467
Remano <remanovictor@icloud.coms>
2019-10-27 00:34:45 +0200

    added files to design 2 ma

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+let hamburger = document.querySelector(".hamburger");
+let content = document.querySelector(".dropContent");
+
+hamburger.addEventListener("click", function() {
+  
+  hamburger.classList.toggle("is-active");
+  
+  if(content.style.display === 'none'){
+    content.style.display='block';
+  }
+  else{
+    content.style.display='none';
+  }
+});
+
+
+
+// ds-ham-nav
+let mobileNav= document.getElementById("hammy");
+
+mobileNav.addEventListener("click", function() {
+  
+    mobileNav.classList.toggle("is-active");
+    
+  });
+
+
+// lazy loading
+
+if ('loading' in HTMLImageElement.prototype) {
+  //Native Lazy Loading Supported!
+
+  console.log('yay!')
+
+  const images = document.querySelectorAll('img[loading="lazy"]')
+  images.forEach(img => {
+      img.src = img.dataset.src;
+  })
+} else {
+  // Not supported!
+
+  console.log('boo!')
+
+  const script = document.createElement('script')
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js';
+  document.body.appendChild(script);
+}
